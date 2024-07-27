970bb52d6fc2522ae0bd0dec12c993b29a588bb0
Fexx745 <Chtupon21396@gmail.com>
2024-07-27 19:30:51 +0700

    แก้ไข index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include ('condb.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>สวัสดี</title>
     <?php include ('script-css.php'); ?>
 
 </head>
