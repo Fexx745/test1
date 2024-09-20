b30d30bc7cf9f11b0893ee9ed1d61f240b471260
Fexx745 <142575597+Fexx745@users.noreply.github.com>
2024-09-20 15:46:09 +0700

    แก้

--- admin/adjust_order.php
+++ admin/adjust_order.php
@@ -7,7 +7,7 @@
     $sql = "UPDATE tb_order SET order_status = 3 WHERE orderID='$idpd' ";
     $result = mysqli_query($conn, $sql);
     if ($result) {
-        $_SESSION['confirmOrder'] = "ยกเลิกคำสั่งซื้อ";
+        $_SESSION['confirmOrder'] = "เปลี่ยนสถาะเป็นการัจดส่ง";
         header('Location: report_order_wait.php');
         exit();
     } else {
--- admin/product_insert.php
+++ admin/product_insert.php
@@ -3,7 +3,6 @@ session_start();
 include('condb.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get form data
     $p_name = $_POST['p_name'];
     $detail = $_POST['detail'];
     $typeprd = $_POST['typeprd'];
@@ -12,25 +11,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $price = $_POST['price'];
     $amount = $_POST['amount'];
 
-    // Check if a file is uploaded
     if ($_FILES['fileimage']['name']) {
-        // Process image upload
-        $targetDir = "../assets/images/product/"; // Change this to the desired upload directory
+        $targetDir = "../assets/images/product/";
         $targetFile = $targetDir . basename($_FILES['fileimage']['name']);
         move_uploaded_file($_FILES['fileimage']['tmp_name'], $targetFile);
         $image = $_FILES['fileimage']['name'];
     } else {
-        // Handle case where no file is uploaded
-        $image = ''; // Set a default value or handle as needed
+        $image = '';
     }
 
-    // Insert product information into the product table
     $insertProductQuery = "INSERT INTO product (p_name, detail, type_id, unit_id, brand_id, amount, image) VALUES ('$p_name', '$detail', '$typeprd', '$unittype', '$brandprd', '$amount', '$image')";
     $resultProduct = mysqli_query($conn, $insertProductQuery);
 
     if ($resultProduct) {
-        // If product insertion is successful, insert price information into price_history table
-        $productId = mysqli_insert_id($conn); // Get the last inserted product id
+        $productId = mysqli_insert_id($conn);
         $insertPriceQuery = "INSERT INTO price_history (p_id, from_date, price) VALUES ('$productId', NOW(), '$price')";
         $resultPrice = mysqli_query($conn, $insertPriceQuery);
 
@@ -47,7 +41,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 } else {
-    // Redirect if accessed directly without form submission
     header("Location: index.php");
     exit();
 }
